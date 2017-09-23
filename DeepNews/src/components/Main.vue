a8f414780b13b495522cb2403a7cb0373caaed5e
Oliver <oliverhi@umich.edu>
2017-09-23 11:42:57 -0400

    Starting search bar

--- DeepNews/src/components/Main.vue
+++ DeepNews/src/components/Main.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <v-toolbar
+      class = "white"
+      floating
+      dense
+      >
+       <v-text-field prepend-icon="search" hide-details single-line></v-text-field>
+    </v-toolbar>
     <google-map>
     </google-map>
   </div>
