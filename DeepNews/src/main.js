52623698f65e709a0744b5b2e90e0c5a49009b0f
Oliver <oliverhi@umich.edu>
2017-09-23 11:26:01 -0400

    Using vuetify for CSS now.

--- DeepNews/src/main.js
+++ DeepNews/src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
+
+// Use Vuetify for CSS library.
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +17,6 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+// So we can use the router everywhere.
+window.router = app.$router
