ee05be80555c4484902959cfb47f57505845dbfa
Jeethi Nair <Jeethi@Jeethis-MacBook-Pro.local>
2017-09-23 15:40:51 -0400

    Work on cards

---
+++ DeepNews/src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout>
+  <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-media>
+          <img src = "static/hills.jpg" height = 200px>
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <a href="https://i.ytimg.com/vi/kfv-1CNomcU/maxresdefault.jpg" target="_blank">{{ msg }}</a>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+ </v-layout>
+
+</template>
+
+
+<script>
+  export default{
+    data(){
+      return{
+        msg: 'Hills hills hills'
+      }
+    }
+  }
+</script>
+
+
+<style>
+div {
+}
+</style>
